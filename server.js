076b1b8b7daf663491f35f74c4924866df390596
Chavi-Kothari <141266025+Chavi-Kothari@users.noreply.github.com>
2023-08-03 18:42:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,20 +8,16 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
-
+import { fileURLToPath } from "url"
 conncetDB();
 const app=express();
-
-//es module file
+//esmodule file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname,'./client/build')))
-
 //routes
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/category",categoryRoute)
@@ -29,10 +25,12 @@ app.use("/api/v1/product",productRoute)
 
 //rest apis
 app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
+
 const PORT = process.env.PORT || 8080;
- 
+
 app.listen(PORT,(req,res)=>{
     console.log(`server listening at ${PORT}`)
 })
+//
