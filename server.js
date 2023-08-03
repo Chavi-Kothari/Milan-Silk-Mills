00812fed4353e98562a30db676e80b6c9e8b1b36
Chavi-Kothari <141266025+Chavi-Kothari@users.noreply.github.com>
2023-08-03 18:35:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import { fileURLToPath } from "url";
 
 conncetDB();
 const app=express();
+
 //es module file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
